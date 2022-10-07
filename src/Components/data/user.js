4723cc9e30a9022e3687e7fd019436b316852c81
Maureen Murugi <maureen.murugi@student.moringaschool.com>
2022-10-07 13:30:22 +0300

    Update the data/user file for props

--- src/Components/data/user.js
+++ src/Components/data/user.js
@@ -4,7 +4,7 @@ const user = {
     fullName: "Maureen Murugi",
     userName: "Murugi",
     email:"murugimaureen99@gmail.com",
-    skill: "A quality-oriented web-developer withan eye of design.",
+    skill: "A quality-oriented web-developer with an eye of design.",
     
 
 
